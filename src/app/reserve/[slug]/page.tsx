f75f2b116f311be37454f8c337cb8370560b35a0
MehmetZiya <mehmetziya82@gmail.com>
2023-02-19 15:02:24 +0100

    next.js 13 restaurant booking app layout created

---
+++ src/app/reserve/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/app/components/Navbar'
+import Form from './components/Form'
+import Header from './components/Header'
+
+export default function ReservationPage() {
+  return (
+    <div className='border-t h-screen'>
+      <div className='py-9 w-3/5 m-auto'>
+        <Header />
+        <Form />
+      </div>
+    </div>
+  )
+}
---
+++ src/app/restaurant/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import Description from './components/Description'
+import Header from './components/Header'
+import Images from './components/Images'
+import Rating from './components/Rating'
+import RestaurantNavbar from './components/RestaurantNavbar'
+import Reviews from './components/Reviews'
+import Title from './components/Title'
+import ReservationCard from './components/ReservationCard'
+
+export default function RestaurantDetails() {
+  return (
+    <>
+      <Header />
+      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+        <div className='bg-white w-[70%] rounded p-3 shadow'>
+          <RestaurantNavbar />
+          <Title />
+          <Rating />
+          <Description />
+          <Images />
+          <Reviews />
+        </div>
+        <div className='w-[27%] relative text-reg'>
+          <ReservationCard />
+        </div>
+      </div>
+    </>
+  )
+}
